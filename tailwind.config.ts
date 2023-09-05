5bca6b3cee7fe27c4838f985446f77604a9c03ca
MrBs <Chouaibwidadipur@gmail.com>
2023-09-06 00:14:41 +0100

    Initial commit from Create Next App

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        VeryDarCyan: "var(--VeryDarCyan)",
+        DarkGrayishCyan: "var(--DarkGrayishCyan)",
+        GrayishCyan: "var(--GrayishCyan)",
+        LightGrayishCyan: "var(--LightGrayishCyan)",
+        VeryLightGrayishCyan: "var(--VeryLightGrayishCyan)",
+        White: "var(--White)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
